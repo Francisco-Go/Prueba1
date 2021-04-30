42eddbf6dc22a56dd23015601efccc31db501913
Francisco-Go <franciscogomezperez998@gmail.com>
2021-04-30 09:58:20 -0500

    se modifico la parte de navbar

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-<h1><?php 
-echo("Hola mundo como estan todos.");
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link type="text/css" rel="stylesheet" href="Assets/css/styles.css"/> 
 
-?></h1>
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+  <a id="nav2" class="navbar-brand" href="#">PortalEmpleo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a id="nav2" class="nav-link submenu-btn dropdown-toggle" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a id="nav2" class="nav-link submenu-btn dropdown-toggle" href="#">Empleos</a>
+      </li>
+      <li class="nav-item">
+        <a id="nav2" class="nav-link submenu-btn dropdown-toggle" href="#">Empleadores</a>
+      </li>
+      <li class="nav-item">
+        <a id="nav2" class="nav-link submenu-btn dropdown-toggle" href="#">Páginas</a>
+      </li>
+      <li class="nav-item">
+        <a id="nav2" class="nav-link submenu-btn dropdown-toggle" href="#">Blog</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+    </span>
+    <button type="button" class="btn btn-primary"><b>Inscribete</b></button>
+    <button type="button" class="btn btn-primary"><b>Acceso</b></button>
+</nav>
